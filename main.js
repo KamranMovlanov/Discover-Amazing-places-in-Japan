adbe570ca057957c011c071fb3beec275ac7ac59
Kamran Movlanov <kamranmovlanov1992@gmail.com>
2021-10-04 18:23:39 +0300

    Reduced code size for burger menu

--- main.js
+++ main.js
@@ -14,15 +14,31 @@
 
 
 
-// Burger menu
+// Burger menu Проанализируй компактный вариант с видео
 
 //SELECTORS
 const burger = document.querySelector(".burger");
 const headerClose = document.querySelector(".header__nav-close");
+const headerNav = document.querySelector(".header__nav");
+
+
+(function burgerMenuAll(){ //Укоротил функцию сверху в итоге занимает в 2 раза меньше места
+    burger.addEventListener('click', () => { //Add
+        headerNav.classList.add("header__nav-active");
+    });
+    headerNav.addEventListener('click', () => {  //Remove
+        headerNav.classList.remove("header__nav-active");
+    });
+
+}());
+
+
+
+
 
 
 //EVENTS
-burger.addEventListener('click', burgerMenu);
+/*burger.addEventListener('click', burgerMenu);
 headerClose.addEventListener('click', burgerClose);
 
 
@@ -30,12 +46,16 @@ headerClose.addEventListener('click', burgerClose);
 
 //FUNCTION
 function burgerMenu(){
-    const headerNav = document.querySelector('.header__nav');
     headerNav.classList.add("header__nav-active");
 }
 
 
 function burgerClose(){
-    const headerNav = document.querySelector('.header__nav');
     headerNav.classList.remove("header__nav-active");
 }
+
+*/
+
+
+
+
