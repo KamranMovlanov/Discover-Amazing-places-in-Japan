b4f6f6cd76da346b94c451054c0062f8bcdb0113
Kamran <81586034+KamranMovlanov@users.noreply.github.com>
2022-05-16 17:37:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,47 +14,21 @@
 }());
 
 
-
 // Burger menu
 (function burgerMenuAll(){
     const burger = document.querySelector(".burger");
     const headerClose = document.querySelector(".header__nav-close");
     const headerNav = document.querySelector(".header__nav");
 
-    
-    burger.addEventListener('click', () => { //Add    
+    //Add    
+    burger.addEventListener('click', () => { 
         headerNav.classList.add("header__nav-active");
     });
-
-    headerClose.addEventListener('click', () => {  //Remove
+    
+    //Remove
+    headerClose.addEventListener('click', () => {  
         headerNav.classList.remove("header__nav-active");
     });
 
 }());
-
-
-
-
-
-/*
-//EVENTS
-burger.addEventListener('click', burgerMenu);
-headerClose.addEventListener('click', burgerClose);
-
-
-
-
-//FUNCTION
-function burgerMenu(){
-    headerNav.classList.add("header__nav-active");
-}
-
-
-function burgerClose(){
-    headerNav.classList.remove("header__nav-active");
-}
-*/
-
-
-
 
